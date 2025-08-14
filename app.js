a1294672028f8a04d5452ae1b101f6c3da33e41f
刘宁 <7610924+liuning1234561@user.noreply.gitee.com>
2025-08-14 10:58:08 +0800

    纠错

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ const jwtAuth = (req, res, next) => {
         req.Token ||
         req.cookies.token ||
         req.headers.authorization?.split(' ')[1]
+    console.log('请求头 Authorization：', req)
     console.log('JWT 验证中间件：', token)
     if (!token) {
         return res.status(401).json({ error: '登录过期，请重新登录' })
